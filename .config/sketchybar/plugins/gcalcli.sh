47fd9cbfacd6b1330c6412e105aea6409493abcf
Dwain Maralack <dwain.maralack@gmail.com>
2024-10-22 13:15:06 +0200

    Update next calender update to use gsed instead of sed and removing everything except the next entries title

--- .config/sketchybar/plugins/gcalcli.sh
+++ .config/sketchybar/plugins/gcalcli.sh
@@ -3,4 +3,4 @@
 # The $NAME variable is passed from sketchybar and holds the name of
 # the item invoking this script:
 # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
-sketchybar --set "$NAME" label="$(gcalcli agenda --nocolor --nostarted --details title | head -2  | tail -1 | sed -r "s/Wed|Mon|Tue|Thu|Fri|Sat|Sun//" | sed -r "s/Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec//" | sed -r 's/[^[:print:]]//g' | sed -r 's/\[0m\[0m//')" \
+sketchybar --set "$NAME" label="$(gcalcli agenda --nocolor --nostarted --details title | head -2  | tail -1 | gsed -e 's/^\w*\s\w*\s\w*.*[0-9]\s*//')"
