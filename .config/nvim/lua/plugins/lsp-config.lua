fd32e573681f9af7abb18dfaacd1c9fda6ea9deb
Dwain Maralack <dwain.maralack@gmail.com>
2025-02-21 19:34:58 +0200

    LSP config issues, updated to ensure that everything works

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,59 +1,95 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config=function()
-			require("mason").setup()
-		end
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts={
-			auto_install=true,
-		}
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require('cmp_nvim_lsp').default_capabilities()
-			local lspconfig = require("lspconfig")
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        build = ":MasonUpdate",
+        config = function()
+            require("mason").setup({
+                ui = {
+                    icons = {
+                        package_installed = "✓",
+                        package_pending = "➜",
+                        package_uninstalled = "✗"
+                    }
+                }
+            })
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        dependencies = {
+            "williamboman/mason.nvim"
+        },
+        config = function()
+            require("mason-lspconfig").setup({
+                auto_install = true,
+                ensure_installed = {
+                    "lua_ls",
+                    "ts_ls",
+                    "solargraph",
+                    "html",
+                    "cssmodules_ls",
+                    "ruby_lsp",
+                    "phpactor"
+                }
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+        },
+        config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities
-			})
-			lspconfig.solargraph.setup({
-				capabilities = capabilities
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities
-			})
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities
-			})
-			lspconfig.phpactor.setup({
-				on_attach = on_attach,
-				capabilities = capabilities,
-				init_options = {
-					["language_server_phpstan.enabled"] = false,
-					["language_server_psalm.enabled"] = false,
-				}
-			})
-			lspconfig.cssmodules_ls.setup({
-				capabilities = capabilities
-			})
-			lspconfig.ruby_lsp.setup({
-				capabilities = capabilities,
-				formatter='standard',
-				linters={'standard'}
-			})
+            local lspconfig = require("lspconfig")
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-		end,
-	},
+            lspconfig.ts_ls.setup({
+                capabilities = capabilities
+            })
+
+            lspconfig.solargraph.setup({
+                capabilities = capabilities
+            })
+
+            lspconfig.html.setup({
+                capabilities = capabilities
+            })
+
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
+
+            lspconfig.phpactor.setup({
+                on_attach = on_attach,
+                capabilities = capabilities,
+                init_options = {
+                    ["language_server_phpstan.enabled"] = false,
+                    ["language_server_psalm.enabled"] = false,
+                }
+            })
+
+            lspconfig.cssmodules_ls.setup({
+                capabilities = capabilities
+            })
+
+            lspconfig.ruby_lsp.setup({
+                capabilities = capabilities,
+                formatter = 'standard',
+                linters = {'standard'}
+            })
+
+            -- LSP Keymaps
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+        end,
+    },
 }
