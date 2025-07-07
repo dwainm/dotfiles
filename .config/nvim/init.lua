266426fc4318d403fc991e5980580bf95d14a6e2
Dwain Maralack <dwain.maralack@gmail.com>
2025-07-07 11:04:49 +0200

    Add custom macros for markdown file.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,3 +3,15 @@ require("config.lazy")
 require("remap")
 require("snippets")
 vim.cmd('colorscheme rose-pine')
+
+
+---------------------
+--- Custom Macros ---
+---------------------
+vim.api.nvim_create_autocmd("FileType",{
+	pattern = {"markdown", "md"},
+	callback= function()
+		local esc = vim.api.nvim_replace_termcodes("<Esc>",true, true, true)
+		vim.fn.setreg("l", "c[]()".. esc .."F[p".. esc .. "f(aurl".. esc )
+	end,
+})
