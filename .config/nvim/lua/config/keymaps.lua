affd06d96385612942bac97b8004cba59558dbc5
Dwain Maralack <dwain.maralack@gmail.com>
2025-10-16 08:49:26 +0200

    Neovim: Add leade f5 to reload safari without leaving the editor.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Safari reload
+vim.keymap.set("n", "<leader><F5>", function()
+  local script = [[
+    tell application "Safari"
+      set theURL to URL of current tab of window 1
+      set URL of current tab of window 1 to theURL
+    end tell
+  ]]
+  local result = vim.fn.system("osascript -e " .. vim.fn.shellescape(script))
+  if vim.v.shell_error == 0 then
+    vim.notify("Safari reloaded", vim.log.levels.INFO)
+  else
+    vim.notify("Failed to reload Safari: " .. result, vim.log.levels.ERROR)
+  end
+end, { desc = "Reload Safari" })
