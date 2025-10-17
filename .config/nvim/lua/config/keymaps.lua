4be23305561fd3c77ff996e2b5d8a4b8ba60af70
Dwain Maralack <dwain.maralack@gmail.com>
2025-10-17 10:23:47 +0200

    Nvim: add LazyGit and improve Git shorcuts to use gitsign for most things, removing the snack_picker actions.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -17,3 +17,22 @@ vim.keymap.set("n", "<leader><F5>", function()
     vim.notify("Failed to reload Safari: " .. result, vim.log.levels.ERROR)
   end
 end, { desc = "Reload Safari" })
+
+-----------
+-- Git
+-----------
+-- Remove uneeded keys
+local maps = { "gb", "gB", "gd", "gf", "gl", "gL", "gs", "gS", "gY" }
+for _, m in ipairs(maps) do
+  vim.keymap.del("n", "<leader>" .. m)
+end
+
+-- Replace with gitsigns (buffer-local, no picker)
+local gs = require("gitsigns") -- Cache for efficiency
+vim.keymap.set("n", "<leader>gb", function()
+  gs.blame_line({ full = true })
+end, { desc = "Git Blame Line" }) -- blame current line
+vim.keymap.set("n", "<leader>gd", gs.preview_hunk, { desc = "Git Diff Hunk" }) -- preview hunk diff
+vim.keymap.set({ "n", "v" }, "<leader>gs", gs.stage_hunk, { desc = "Git Stage Hunk" }) -- stage hunk
+vim.keymap.set("n", "<leader>gS", gs.stage_buffer, { desc = "Git Stage Buffer" }) -- stage buffer
+vim.keymap.set({ "n", "v" }, "<leader>gr", gs.reset_hunk, { desc = "Git Reset Hunk" }) -- reset hunk
