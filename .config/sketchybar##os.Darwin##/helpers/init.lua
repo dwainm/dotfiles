a0ebb3fd0d4265ed99b359251d1f7630f2bb7542
Dwain Maralack <dwain.maralack@gmail.com>
2026-05-20 08:07:40 +0200

    Remove sketchybar - unload service, delete config dir, clean references in skhdrc and aerospace.toml

--- .config/sketchybar##os.Darwin##/helpers/init.lua
+++
@@ -1,4 +0,0 @@
--- Add the sketchybar module to the package cpath
-package.cpath = package.cpath .. ";/Users/" .. os.getenv("USER") .. "/.local/share/sketchybar_lua/?.so"
-
-os.execute("make -C $HOME/.config/sketchybar/helpers -q 2>/dev/null || make -C $HOME/.config/sketchybar/helpers")
